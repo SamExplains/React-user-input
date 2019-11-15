5faf0ab5a23d358d24d4bcbe927220f7ad875a98
Sampuccino <Sampuccino@gmail.com>
2019-11-14 18:32:13 -0800

    Updated SearchBar component to emit event to the parent App component to do the data requesting.

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -4,16 +4,22 @@ class Searchbar extends React.Component{
 
   state = { term: ''};
 
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return <div className='ui segment'>
-            <form className='ui form'>
+            <form className='ui form' onSubmit={this.onFormSubmit}>
               <div className='field'>
                 <label>Image Search</label>
-                <input value={this.state.term} onChange={(e) => this.setState({ term: e.target.value }) } type="text"/>
+                <input value={this.state.term} onChange={(e) => this.setState({ term: e.target.value.toLocaleUpperCase()}) } type="text"/>
               </div>
             </form>
           </div>
   }
+
 }
 
 export default Searchbar;
